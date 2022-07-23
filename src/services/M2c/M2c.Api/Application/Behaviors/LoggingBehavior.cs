06ac5d04be65ae5cca55e589046c32b43a5f2947
reza <rezafaghani@live.com>
2022-07-23 18:01:43 +0430

    add-validation-behavior add-log-behavior add-generic-type-extension

---
+++ src/services/M2c/M2c.Api/Application/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using M2c.Infrastructure.Extensions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace M2c.Api.Application.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            _logger.LogInformation("----- Handling command {CommandName} ({@Command})", request.GetGenericTypeName(),
+                request);
+            var response = await next();
+            _logger.LogInformation("----- Command {CommandName} handled - response: {@Response}",
+                request.GetGenericTypeName(), response);
+
+            return response;
+        }
+    }
+}
+
